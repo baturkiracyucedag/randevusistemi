52ebb5511d00c0bb8a15495a1e568d1c4224fc7a
Batur Kıraç Yücedağ <kirac233@gmail.com>
2026-07-23 21:19:01 +0300

    Hizmet repository oluşturuldu

---
+++ src/main/java/com/kirac/randevusistemi/repository/HizmetRepository.java
@@ -0,0 +1,9 @@
+package com.kirac.randevusistemi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kirac.randevusistemi.entity.Hizmet;
+
+public interface HizmetRepository extends JpaRepository<Hizmet, Integer> {
+
+}
